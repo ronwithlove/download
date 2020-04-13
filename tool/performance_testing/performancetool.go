4ac26ddd383e4da1c00be69bb2797a82aa8ef429
YIMING SHI <shiyiming@outlook.com>
2020-04-13 12:46:27 -0700

    zip tools

---
+++ tool/performance_testing/performancetool.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/dappley/go-dappley/tool/performance_testing/service"
+	"time"
+)
+
+const goCount 		= 5//go程数量
+
+
+func main() {
+	dappSdk := service.NewServiceClient()
+
+	for i:=0;i<goCount;i++{
+		dappSdk.StartTransactionGoroutine(i+1)
+			time.Sleep(5 * time.Second)
+	}
+	//todo:更新方式需要改一下
+
+	neverQuit := make(chan bool)
+	<-neverQuit
+
+}
+
